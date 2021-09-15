8c0917f45c416c3249297b79013d70a3b5c59e27
Andrea Rogers <targetdisk1394@gmail.com>
2021-09-15 19:26:30 -0400

    added note to setup.py
    No idea why install_requires sucks so hard

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ try:
 except ImportError:
     from pip._internal import main as pipmain
 
+# install_requires breaks installing bluepy.  I don't know thy and I really
+# don't care, so I did this:
 pipmain(["install", "-r", "requirements.txt"])
 
 scripts = ["scripts/enable-sc-wifi"]
